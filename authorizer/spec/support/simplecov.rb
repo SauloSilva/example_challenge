5d36ca04cea4fff71f287e44b20f95cf38b55e14
Saulo Santiago <saulodasilvasantiago@gmail.com>
2020-09-19 03:32:59 -0300

    Improves support file of spec

--- authorizer/spec/support/simplecov.rb
+++ authorizer/spec/support/simplecov.rb
@@ -7,9 +7,13 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
 SimpleCov.start do
   add_filter '/spec/'
 
+  add_group 'Controllers', 'app/layers/web/controllers'
   add_group 'Domains', 'app/layers/domain'
+  add_group 'Application', 'app/layers/application'
   add_group 'Infra/Events', 'app/layers/infra/events'
   add_group 'Infra/Consumers', 'app/layers/infra/consumers'
+  add_group 'Infra/Repositories', 'app/layers/infra/repositories'
+  add_group 'Infra/API/serializers', 'app/layers/infra/api/serializers'
   add_group 'Lib', 'lib'
   add_group 'DB', 'db'
 end
