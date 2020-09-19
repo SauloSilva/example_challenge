ca2276d498d5ff73c2613b076990659c8663355b
Saulo Santiago <saulodasilvasantiago@gmail.com>
2020-09-19 03:37:28 -0300

    Improves parameters of account repository

--- authorizer/app/layers/infra/repositories/account_repository.rb
+++ authorizer/app/layers/infra/repositories/account_repository.rb
@@ -1,8 +1,8 @@
 module Infra
   module Repositories
     class AccountRepository
-      def new_record(account_command)
-        Domain::Account::Account.new(account_command.params)
+      def new_record(params)
+        Domain::Account::Account.new(params)
       end
 
       def find_last
