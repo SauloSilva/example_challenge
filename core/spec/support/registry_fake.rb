65ed0b94372ff1e765bff6d4db2163c71c427f7a
Saulo Santiago <saulodasilvasantiago@gmail.com>
2020-09-22 03:58:45 -0300

    Add new url of webmock on core library

--- core/spec/support/registry_fake.rb
+++ core/spec/support/registry_fake.rb
@@ -2,6 +2,6 @@ require 'webmock/rspec'
 
 RSpec.configure do |config|
   config.before(:each) do
-    WebMock.stub_request(:post, 'https://eny44nhjua6vj5d.m.pipedream.net').to_return(body: "ok")
+    WebMock.stub_request(:post, 'https://4eb16a200c5e4f95ffa4b36d02c58a4b.m.pipedream.net').to_return(body: "ok")
   end
 end
